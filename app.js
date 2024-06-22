ac5861432a1091287c30048009c3867ea01562b6
DannyStalter <dan.r.stalter@gmail.com>
2024-06-22 14:37:09 -0400

    HTML clean up. Project hover animation lessened.

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-var danSmall = document.querySelector('.dan-small');
-var danLarge = document.querySelector('.dan-large');
-var hamburger = document.querySelector('.hamburger');
-var navLinks = document.querySelector('.navlinks');
-
-//Hamburger menu open/close
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('active');
-    navLinks.classList.toggle('active');
-});
-
-//Nav Links close menu on click
-document.querySelectorAll('.navlinks').forEach(n => n.addEventListener('click', function() {
-    hamburger.classList.remove('active');
-    navLinks.classList.remove('active');
-}));
-
-//Responsive picture resizing
-function myFunction(x) {
-    if (x.matches) { // If media query matches
+document.addEventListener('DOMContentLoaded', () => {
+    const danSmall = document.querySelector('.dan-small');
+    const danLarge = document.querySelector('.dan-large');
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.navlinks');
+  
+    // Hamburger menu open/close
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('active');
+      navLinks.classList.toggle('active');
+    });
+  
+    // Nav Links close menu on click
+    navLinks.querySelectorAll('a').forEach(link => link.addEventListener('click', () => {
+      hamburger.classList.remove('active');
+      navLinks.classList.remove('active');
+    }));
+  
+    // Responsive picture resizing
+    const resizePictures = x => {
+      if (x.matches) {
         danSmall.classList.add('hide');
         danLarge.classList.remove('hide');
-    } else {
+      } else {
         danSmall.classList.remove('hide');
         danLarge.classList.add('hide');
+      }
     }
-  }
   
-  var x = window.matchMedia("(min-width: 978px)")
-  myFunction(x) // Call listener function at run time
-  x.addListener(myFunction) // Attach listener function on state changes - depreciated but listed in W3Schools
+    const x = window.matchMedia("(min-width: 978px)");
+    resizePictures(x);
+    x.addListener(resizePictures);
+  });
+  
